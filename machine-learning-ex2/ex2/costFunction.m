e1d9d5b5e1e152eeb0a386e733f60d68590135ae
Andrea Lianto <andreamarsha@gmail.com>
2018-11-08 21:31:56 +0000

    Add cost and gradient functions

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,8 +20,15 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Calculate sigmoid function
+z = X * theta;
+h = sigmoid(z);
 
+% Calculate cost
+J = (-1/m) * ((y' * log(h)) + ((1-y)' * log(1-h)));
 
+% Calculate gradient
+grad = (1/m) * (X' * (h-y));
 
 
 
